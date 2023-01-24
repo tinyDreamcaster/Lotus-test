d5b21ec21bd55dbda5cc80f61cbbe2d8f7c56bd7
tinyDreamcaster <frunicefrun@gmail.com>
2023-01-25 02:44:08 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Countdown, { zeroPad } from 'react-countdown';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
@@ -8,13 +8,13 @@ function App() {
 
 
 
-  const testRef = useRef();
+
 
   const users = [
     { id: 1, username: 'USER1', activities: "-", productionTime: '80', warranty: '24', paymentTerms: '30%', lotPrice: 3700000, lotPrice2: -25000 },
-    { id: 2, username: 'USER2', activities: "-", productionTime: '90', warranty: '24', paymentTerms: '100%', lotPrice: 3200000, lotPrice2: -25000},
-    { id: 3, username: 'USER3', activities: "-", productionTime: '75', warranty: '22', paymentTerms: '60%', lotPrice: 2800000, lotPrice2: -25000},
-    { id: 4, username: 'USER4', activities: "-", productionTime: '120', warranty: '36', paymentTerms: '50%', lotPrice: 2500000, lotPrice2: -25000},
+    { id: 2, username: 'USER2', activities: "-", productionTime: '90', warranty: '24', paymentTerms: '100%', lotPrice: 3200000, lotPrice2: -25000 },
+    { id: 3, username: 'USER3', activities: "-", productionTime: '75', warranty: '22', paymentTerms: '60%', lotPrice: 2800000, lotPrice2: -25000 },
+    { id: 4, username: 'USER4', activities: "-", productionTime: '120', warranty: '36', paymentTerms: '50%', lotPrice: 2500000, lotPrice2: -25000 },
   ]
 
   const Completionist = () => {
@@ -48,19 +48,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Ход торгов Тестовые торги на аппарат ЛОТОС №2033564 (09.11.2020 07:00)</h1>
-      <p>Уважаемые участники, во время вашего хода вы можете изменить параметры торгов, указанных в таблице:</p>
+      <h1 className='pageTitle'>Ход торгов <b>Тестовые торги на аппарат ЛОТОС №2033564 (09.11.2020 07:00)</b></h1>
+      <p className='pageSubtitle'>Уважаемые участники, во время вашего хода вы можете изменить параметры торгов, указанных в таблице:</p>
 
       <table className='aucTable'>
-        <tbody>
+        <thead>
           <tr>
-            <td> Ход </td>
+            <td className='aucTable__title'> Ход </td>
             {
               users.map((user, index) =>
                 index == currentUser
                   ?
-                  <td ref={testRef}>
-                    <Countdown date={Date.now() + 5000}
+                  <td className='aucTable__timer'>
+                    <Countdown date={Date.now() + 120000}
                       renderer={renderer}
                     />
                   </td>
@@ -72,16 +72,18 @@ function App() {
           </tr>
 
           <tr>
-            <td> Параметры и требования </td>
+            <td className='aucTable__title aucTable__header'> Параметры и требования </td>
             {
               users.map((user, index) =>
-                <td> <p>Участник {index + 1}</p> <p>{user.username}</p> </td>
+                <td className='aucTable__title aucTable__header'> <p>Участник №{index + 1}</p> <p>{user.username}</p> </td>
               )
             }
           </tr>
 
+        </thead>
 
-          <tr>
+        <tbody className='aucTable__body'>
+          <tr className='aucTable__cells aucTable__activities'>
             <td> Наличие комплекса мероприятий, повышающих стандарты качества изготовления </td>
             {
               users.map((user, index) =>
@@ -91,7 +93,7 @@ function App() {
 
           </tr>
 
-          <tr>
+          <tr className='aucTable__cells'>
             <td> Срок изготовления лота, дней </td>
             {
               users.map((user, index) =>
@@ -100,7 +102,7 @@ function App() {
             }
           </tr>
 
-          <tr>
+          <tr className='aucTable__cells'>
             <td> Гарантийные обязательства, мес </td>
             {
               users.map((user, index) =>
@@ -109,7 +111,7 @@ function App() {
             }
           </tr>
 
-          <tr>
+          <tr className='aucTable__cells'>
             <td> Условия оплаты </td>
             {
               users.map((user, index) =>
@@ -118,14 +120,14 @@ function App() {
             }
           </tr>
 
-          <tr>
+          <tr className='aucTable__cells'>
             <td> Стоимость изготовления лота, руб (Без НДС) </td>
             {
               users.map((user, index) =>
-                <td>
-                  <p>{user.lotPrice}</p>
-                  <p>{user.lotPrice2}</p>
-                  <p>{user.lotPrice - user.lotPrice2}</p>
+                <td className='aucTable__price'>
+                  <p className='priceReduced'><b>{user.lotPrice} руб.</b></p>
+                  <p  className='priceSubtracted'><b>{user.lotPrice2} руб.</b></p>
+                  <p  className='priceDifference'><b>{user.lotPrice - user.lotPrice2} руб.</b></p>
                 </td>
               )
             }
@@ -133,7 +135,7 @@ function App() {
         </tbody>
       </table>
 
-      <p>Действия:</p>
+      <p className='actions'>Действия:</p>
     </div>
   );
 }
